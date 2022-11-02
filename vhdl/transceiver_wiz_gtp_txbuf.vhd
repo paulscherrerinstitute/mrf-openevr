650146e0673584b89de4527c45ff3ed06c1d48ca
till straumann <till.straumann@alumni.tu-berlin.de>
2022-11-02 15:20:10 +0100

    changed pll default parameters
    pstep: 1
    istep: 1
    ishft: 3
    decm : 4

--- vhdl/transceiver_wiz_gtp_txbuf.vhd
+++ vhdl/transceiver_wiz_gtp_txbuf.vhd
@@ -276,9 +276,9 @@ begin
     signal freqmax           : signed(15 downto 0) := to_signed( 5120, 16);
     signal fmod              : signed(15 downto 0) := (others => '0');
 
-    signal decm              : unsigned(15 downto 0) := to_unsigned(15, 16);
-    signal pstep             : signed  (15 downto 0) := to_signed  ( 2, 16);
-    signal istep             : signed  (15 downto 0) := to_signed  ( 0, 16);
+    signal decm              : unsigned(15 downto 0) := to_unsigned( 4, 16);
+    signal pstep             : signed  (15 downto 0) := to_signed  ( 1, 16);
+    signal istep             : signed  (15 downto 0) := to_signed  ( 1, 16);
     signal ishft             : unsigned(15 downto 0) := to_unsigned( 3, 16);
     signal mshft             : unsigned(15 downto 0) := to_unsigned(15, 16);
     signal piVcoRst          : std_logic := '1';
